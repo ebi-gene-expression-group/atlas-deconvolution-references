7460fa87e6bc856e296cdf5f8654493e61ab8c9e
Nadja Nolte <103439290+nadjano@users.noreply.github.com>
2023-03-28 12:12:15 +0100

    Added some comments and removed whitespace

--- scripts/makeSummary.R
+++ scripts/makeSummary.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-
+### script to create a summary file of all the organism part references we created
 
 # Load required packages
 suppressMessages(library(devtools))
@@ -8,18 +8,19 @@ suppressMessages(library(scOntoMatch))
 suppressMessages(library(ontologyIndex))
 suppressMessages(library(stringr))
 
-
 args <- commandArgs(trailingOnly = TRUE)
 
+# read in list of UMAP.pngs we created
 sample_list <- args[1:(length(args)-1)]
 outfile <- args[length(args)]
 
 if (!file.exists(outfile)) {
-  write.table(data.frame(Accession = character(), UBERON_ID = character(), organism_part = character(), date = character()),
-                        outfile, sep = "\t", row.names = FALSE, col.names = TRUE)
+  write.table(data.frame(Accession = character(), UBERON_ID = character(), 
+                         organism_part = character(), date = character()),
+              outfile, sep = "\t", row.names = FALSE, col.names = TRUE)
 }
 
-#read in UBERON ontologys
+#read in UBERON ontologys to translate UBERON ids to organism part labels
 obo_file = 'deconvolution-reference-library/files/basic.obo' #download here: http://purl.obolibrary.org/obo/uberon/basic.obo
 propagate_relationships = c('is_a', 'part_of', 'relationship: part_of', 'synonym')
 # create UBRERON ontology object
@@ -27,6 +28,7 @@ ont <- ontologyIndex::get_OBO(obo_file,
                               propagate_relationships = propagate_relationships, 
                               extract_tags = 'everything')
 
+#append the summary file for all the references
 for (filename in sample_list){
 
     base = basename(filename)
@@ -34,15 +36,11 @@ for (filename in sample_list){
     organism_part = unlist(getOntologyName(ont = ont, sub('_', ':', uberon_id)))
     accession = str_extract(base, "E-[A-Z]+-\\d+")
     
-    print(getOntologyName(ont = ont, get_descendants(ont, roots= sub('_', ':', uberon_id))))
     # Append a line with the accession and UBERON ID to the TSV file
-    new_line <- data.frame(Accession = accession, UBERON_ID = uberon_id, organism_part = organism_part, date = Sys.Date())
+    new_line <- data.frame(Accession = accession, 
+                           UBERON_ID = uberon_id, 
+                           organism_part = organism_part, 
+                           date = Sys.Date())
     write.table(new_line, outfile, sep = "\t", row.names = FALSE, col.names = FALSE, append = TRUE)
 
 }
-
-
-
-
-
-
