de40565502a0212fefbdf1d2bb00b864dde3bb04
Nadja Nolte <103439290+nadjano@users.noreply.github.com>
2023-03-29 16:29:34 +0100

    improve var names

--- scripts/createSeuratObjectFromANND.R
+++ scripts/createSeuratObjectFromANND.R
@@ -8,40 +8,43 @@ suppressMessages(library(plyr))
 suppressMessages(library(stringr))
 
 args <- commandArgs(trailingOnly = TRUE)
-sample <- args[1]
+if(length(args) != 1) {
+   stop("Not correct number of arguments. Please supply 1 argument")
+}
+accession = args
 
 #filename of seuratObject
-filename_O = paste0('scxa_input/',sample,'/', sample,  '.project_seurat.rds')
+SeuratObject_filename = paste0('scxa_input/',accession,'/', accession, '.project_seurat.rds')
 
 #read in counts
-counts = ReadMtx(mtx = paste0('scxa_input/',sample,'/matrix.mtx.gz'), 
-        features= paste0('scxa_input/',sample, '/genes.tsv.gz'), #genes should be ENS ids without .\d
-        cells=   paste0('scxa_input/',sample,'/barcodes.tsv.gz'),  feature.column = 1)
+counts = ReadMtx(mtx = paste0('scxa_input/',accession ,'/matrix.mtx.gz'), 
+        features= paste0('scxa_input/',accession , '/genes.tsv.gz'), #genes should be ENS ids without .\d
+        cells=   paste0('scxa_input/',accession ,'/barcodes.tsv.gz'),  feature.column = 1)
 
 #read in metadata for cells
-metadata = read.csv(paste0('scxa_input/',sample,'/', sample, '.cell_metadata.tsv'), 
+metadata = read.csv(paste0('scxa_input/',accession,'/', accession, '.cell_metadata.tsv'), 
                         sep = '\t', row.names = 1) #row.names should be cellIDs
 
-C = CreateSeuratObject(counts = counts, meta.data = metadata)
+SeuratObject = CreateSeuratObject(counts = counts, meta.data = metadata)
 
 # rename colum names. If "inferred_cell_type_._ontology_labels_ontology" is 
 # not present in metadata, try C$inferred_cell_type_._authors_labels_ontology 
 # we want to extract the UBERON ids from the links --> basename
-first_try <- try(C$cellType
-                 <- as.factor(basename(as.character(C$authors_cell_type_._ontology_labels_ontology))))
+first_try <- try(SeuratObject$cellType
+                 <- as.factor(basename(as.character(SeuratObject$authors_cell_type_._ontology_labels_ontology))))
 if("try-error" %in% class(first_try)){
-    seconf_try <- try(C$cellType
-                     <- as.factor(basename(as.character(C$authors_cell_type_ontology)))
+    seconf_try <- try(SeuratObject$cellType
+                     <- as.factor(basename(as.character(SeuratObject$authors_cell_type_ontology)))
 }
 if("try-error" %in% class(second_try)){
-    stop(paste(sample, 'does not seem to have ontology cell type labels, but this is a requirement to create references'))
+    stop(paste(accession, 'does not seem to have ontology cell type labels, but this is a requirement to create references'))
 }
 #add new columns
-C$tissue = as.factor(basename(as.character(C$organism_part_ontology)))
-C$cellID = colnames(C)
+SeuratObject$tissue = as.factor(basename(as.character(SeuratObject$organism_part_ontology)))
+SeuratObject$cellID = colnames(SeuratObject)
 
 # Remove cells without CL and UBERON IDs
-C <- C[, grepl("CL", C$cellType) & grepl("UBERON", C$tissue)]
+SeuratObject <- SeuratObject[, grepl("CL", SeuratObject$cellType) & grepl("UBERON", SeuratObject$tissue)]
 
 #save SeuratObject
-saveRDS(C, filename_O)
+saveRDS(SeuratObject, SeuratObject_filename)
