330dde803fc4c5ba2083081385f18679a8bf9c85
Nadja Nolte <103439290+nadjano@users.noreply.github.com>
2023-06-29 16:17:43 +0100

    get cell metadata from condensed sdrf
    cell-metadata file has not all zoom mappings sometimes therefore change to condensed sdrf file.

--- scripts/createSeuratObject.R
+++ scripts/createSeuratObject.R
@@ -4,6 +4,7 @@
 suppressMessages(library(Seurat))
 suppressMessages(library(plyr))
 suppressMessages(library(stringr))
+suppressMessages(library(tidyr))
 
 args <- commandArgs(trailingOnly = TRUE)
 
@@ -15,34 +16,64 @@ species = args[2]
 mtx_path = args[3]
 cols_path = args[4]
 rows_path = args[5]
-cells_path = args[6]
+sdrf_file = args[6]
+
+get_cell_metadata <- function(sdrf_file) {
+  # Read the condensed SDRF file
+  data <- read.csv(sdrf_file, sep = "\t", header = FALSE)
+  
+  # Pivot the dataframe
+  pivoted_df <- data.frame(pivot_wider(data, names_from = c(V4, V5), values_from = V7, id_cols = V3))
+  
+  # Set the row names
+  row.names(pivoted_df) <- pivoted_df$V3
+  
+  # Remove the unnecessary column
+  pivoted_df <- pivoted_df[, -1]
+  
+  # Function to check if a row contains URLs matching specific patterns
+  contains_matching_urls <- function(row) {
+    # Specify the regular expression pattern
+    pattern <- "(http://purl.obolibrary.org/obo/CL_\\d+)|(http://purl.obolibrary.org/obo/UBERON_\\d+)"
+    
+    # Check if any element in the row matches the pattern
+    any(grepl(pattern, row))
+  }
+  
+  # Get the row indices that contain matching URLs
+  matching_rows <- which(apply(pivoted_df, 1, contains_matching_urls))
+  
+  # Return the cell metadata dataframe
+  return(pivoted_df[matching_rows, ])
+}
+
+# get cell metadata file from condensed sdrf file
+cell_metadata <- get_cell_metadata(sdrf_file)
 
 SeuratObject_filename = paste0('scxa_input/', species, '/', accession,'/', accession, '.project_seurat.rds')
 #read in counts
 counts = ReadMtx(mtx = mtx_path, 
                  features = rows_path, 
                  cells = cols_path)
-#read in metadata for cells
-metadata = read.csv(cells_path, sep = '\t', row.names = 1)
 
 #filter only cells that have metadata
-counts = counts[,colnames(counts) %in% rownames(metadata)]
+counts = counts[,colnames(counts) %in% rownames(cell_metadata)]
 
 #create SeuratObject
-SeuratObject = CreateSeuratObject(counts = counts, meta.data = metadata)
+SeuratObject = CreateSeuratObject(counts = counts, meta.data = cell_metadata)
 #rename colum names. If "inferred_cell_type_._ontology_labels_ontology"
 # is not present in metadata, try SeuratObject$inferred_cell_type_._authors_labels_ontology 
 #we want to extract the UBERON ids from the links --> basename
 first_try <- try(SeuratObject$cellType
-                 <- as.factor(basename(as.character(SeuratObject$inferred_cell_type_._ontology_labels_ontology))))
+                 <- as.factor(basename(as.character(SeuratObject$characteristic_cell.type))))
 if("try-error" %in% class(first_try)){
     second_try <- try(SeuratObject$cellType
-                     <- as.factor(basename(as.character(SeuratObject$authors_cell_type_ontology))))
+                     <- as.factor(basename(as.character(SeuratObject$factor_inferred.cell.type...ontology.labels))))
    if("try-error" %in% class(second_try)){
          stop(paste(accession, 'does not seem to have ontology cell type labels, but this is a requirement to create references'))
    }
  }
-SeuratObject$tissue = as.factor(basename(as.character(SeuratObject$organism_part_ontology)))
+SeuratObject$tissue = as.factor(basename(as.character(SeuratObject$characteristic_organism.part)))
 SeuratObject$cellID = colnames(SeuratObject)
 
 # Remove cells without CL and UBERON IDs
